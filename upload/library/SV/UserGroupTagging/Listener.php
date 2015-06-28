59dd0d2fa0252f9c6796c4d962ef72feadb49ba5
Xon <tazipper1@hotmail.com>
2015-06-28 18:19:50 +0800

    Transactions around install

--- upload/library/SV/UserGroupTagging/Listener.php
+++ upload/library/SV/UserGroupTagging/Listener.php
@@ -6,11 +6,16 @@ class SV_UserGroupTagging_Listener
 
     public static function install($installedAddon, array $addonData, SimpleXMLElement $xml)
     {
+        $db = XenForo_Application::getDb();
+        XenForo_Db::beginTransaction($db);
+
         SV_UserGroupTagging_Install::addColumn('xf_user_group', 'sv_taggable', 'tinyint(1) NOT NULL default 0');
         SV_UserGroupTagging_Install::addColumn('xf_user_group', 'sv_private', 'tinyint(1) NOT NULL default 0');
         SV_UserGroupTagging_Install::addColumn('xf_user_group', 'sv_avatar_s', 'text');
         SV_UserGroupTagging_Install::addColumn('xf_user_group', 'sv_avatar_l', 'text');
         SV_UserGroupTagging_Install::addColumn('xf_user_option', 'sv_emailOnTag', 'tinyint(1) NOT NULL default 0');
+
+        XenForo_Db::commit($db);
 
         //"update xf_user_option
         //set  sv_emailOnTag = bdtagme_email ;"
@@ -18,11 +23,16 @@ class SV_UserGroupTagging_Listener
 
     public static function uninstall()
     {
+        $db = XenForo_Application::getDb();
+        XenForo_Db::beginTransaction($db);
+
         SV_UserGroupTagging_Install::dropColumn('xf_user_group', 'sv_taggable');
         SV_UserGroupTagging_Install::dropColumn('xf_user_group', 'sv_avatar_s');
         SV_UserGroupTagging_Install::dropColumn('xf_user_group', 'sv_avatar_l');
         SV_UserGroupTagging_Install::dropColumn('xf_user_group', 'sv_private');
         SV_UserGroupTagging_Install::dropColumn('xf_user_option', 'sv_emailOnTag');
+
+        XenForo_Db::commit($db);
     }
 
     public static function load_class($class, array &$extend)
