657f386662a9f3a61cf6a3819553a19ede264fdc
Xon <tazipper1@hotmail.com>
2015-06-30 08:57:27 +0800

    Tweak editor usergroup support

---
+++ upload/library/SV/UserTaggingImprovements/XenForo/Html/Renderer/BbCode.php
@@ -0,0 +1,14 @@
+<?php
+
+class SV_UserTaggingImprovements_XenForo_Html_Renderer_BbCodet extends XFCP_SV_UserTaggingImprovements_XenForo_Html_Renderer_BbCode
+{
+    public function handleTagA($text, XenForo_Html_Tag $tag)
+    {
+        $group_id = intval($tag->attribute('data-usergroup'));
+        if ($group_id)
+        {
+            return "[USERGROUP={$group_id}]{$text}[/USERGROUP]";
+        }
+        return parent::handleTagA($text, $tag);
+    }
+}
