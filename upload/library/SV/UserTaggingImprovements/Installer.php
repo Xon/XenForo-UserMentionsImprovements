fde5643ff6b2a0f43f5306cb6e1137dfa8b4a5d0
Xon <tazipper1@hotmail.com>
2015-09-07 19:50:57 +0800

    Remove leftover permissions

--- upload/library/SV/UserTaggingImprovements/Installer.php
+++ upload/library/SV/UserTaggingImprovements/Installer.php
@@ -5,15 +5,12 @@ class SV_UserTaggingImprovements_Installer
     public static function install($installedAddon, array $addonData, SimpleXMLElement $xml)
     {
         $db = XenForo_Application::getDb();
-        XenForo_Db::beginTransaction($db);
 
         SV_Utils_Install::addColumn('xf_user_group', 'sv_taggable', 'tinyint(3) NOT NULL default 0');
         SV_Utils_Install::addColumn('xf_user_group', 'sv_private', 'tinyint(3) NOT NULL default 0');
         SV_Utils_Install::addColumn('xf_user_group', 'sv_avatar_s', 'text');
         SV_Utils_Install::addColumn('xf_user_group', 'sv_avatar_l', 'text');
         SV_Utils_Install::addColumn('xf_user_option', 'sv_email_on_tag', 'tinyint(3) NOT NULL default 0');
-
-        XenForo_Db::commit($db);
 
         //"update xf_user_option
         //set  sv_email_on_tag = bdtagme_email ;"
@@ -22,14 +19,28 @@ class SV_UserTaggingImprovements_Installer
     public static function uninstall()
     {
         $db = XenForo_Application::getDb();
-        XenForo_Db::beginTransaction($db);
+
+        $db->query("
+            DELETE FROM xf_permission_entry
+            WHERE permission_group_id = 'forum' and permission_id = 'sv_DisableTagging'
+        ");
+        $db->query("
+            DELETE FROM xf_permission_entry
+            WHERE permission_group_id = 'general' and permission_id = 'sv_ReceiveTagAlertEmails'
+        ");
+        $db->query("
+            DELETE FROM xf_permission_entry
+            WHERE permission_group_id = 'general' and permission_id = 'sv_TagUserGroup'
+        ");
+        $db->query("
+            DELETE FROM xf_permission_entry
+            WHERE permission_group_id = 'general' and permission_id = 'sv_ViewPrivateGroups'
+        ");
 
         SV_Utils_Install::dropColumn('xf_user_group', 'sv_taggable');
         SV_Utils_Install::dropColumn('xf_user_group', 'sv_avatar_s');
         SV_Utils_Install::dropColumn('xf_user_group', 'sv_avatar_l');
         SV_Utils_Install::dropColumn('xf_user_group', 'sv_private');
         SV_Utils_Install::dropColumn('xf_user_option', 'sv_email_on_tag');
-
-        XenForo_Db::commit($db);
     }
 }
