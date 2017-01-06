43d8986713ab22da6733b7dce3e9fb016a359258
Xon <tazipper1@hotmail.com>
2017-01-06 14:31:24 +0800

    Improve compatibility of injecting group information

--- upload/library/SV/UserTaggingImprovements/XenForo/Model/Alert.php
+++ upload/library/SV/UserTaggingImprovements/XenForo/Model/Alert.php
@@ -6,4 +6,21 @@ class SV_UserTaggingImprovements_XenForo_Model_Alert extends XFCP_SV_UserTagging
     {
         return $this->_getAlertHandlerFromCache($contentType);
     }
+
+    public function alertUser($alertUserId, $userId, $username, $contentType, $contentId, $action, array $extraData = null)
+    {
+        if (!empty(SV_UserTaggingImprovements_Globals::$AlertedUsersExtraInfo[$alertUserId]['taggedGroupId']))
+        {
+            if ($extraData === null)
+            {
+                $extraData = array();
+            }
+            $extraData['taggedGroupId'] = SV_UserTaggingImprovements_Globals::$AlertedUsersExtraInfo[$alertUserId]['taggedGroupId'];
+            $extraData['taggedGroup'] = SV_UserTaggingImprovements_Globals::$AlertedUsersExtraInfo[$alertUserId]['taggedGroup'];
+            unset(SV_UserTaggingImprovements_Globals::$AlertedUsersExtraInfo[$alertUserId]);
+
+        }
+
+        return parent::alertUser($alertUserId, $userId, $username, $contentType, $contentId, $action, $extraData);
+    }
 }
