8311a6d97e529af9acc4038b3cb78361a2244825
Xon <tazipper1@hotmail.com>
2018-03-01 02:58:19 +0800

    Fix sending mentions emails from Report Comments.

--- upload/library/SV/UserTaggingImprovements/SV/ReportImprovements/AlertHandler/ReportComment.php
+++ upload/library/SV/UserTaggingImprovements/SV/ReportImprovements/AlertHandler/ReportComment.php
@@ -1,6 +1,6 @@
 <?php
 
-class SV_UserTaggingImprovements_SV_ReportImprovements_AlertHandler_Report extends XFCP_SV_UserTaggingImprovements_SV_ReportImprovements_AlertHandler_Report
+class SV_UserTaggingImprovements_SV_ReportImprovements_AlertHandler_ReportComment extends XFCP_SV_UserTaggingImprovements_SV_ReportImprovements_AlertHandler_ReportComment
 {
     public function getContentUrl(array $content, $canonical = false)
     {
